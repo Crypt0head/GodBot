34d15f171c28c8dd5df6a0cb17625897d1fe26a9
Crypt0head <zelas.money@yandex.ru>
2024-06-26 19:31:12 +0300

    ongoing changes

--- hpp/connection.hpp
+++ hpp/connection.hpp
@@ -1,14 +1,22 @@
 #pragma once
 
 #include <iostream>
+#include <ostream>
 #include <string>
 #include <map>
+#include <list>
 #include <stdexcept>
 
-#include "curl_object.hpp"
+#include <boost/asio.hpp>
+
+// #include "curl_object.hpp"
+
+namespace asio = boost::asio;
+namespace ip = boost::asio::ip;
 
 using json_data = std::string;
 using headers_t = std::map<std::string, std::string>;
+using slist = std::list<std::string>;
 
 namespace http {
 
@@ -24,7 +32,7 @@ namespace http {
 		request() {}
 		virtual ~request() {}
 
-		virtual curl_slist* prepare(const headers_t&) const = 0;
+		virtual void prepare(const headers_t&, std::ostream&) const = 0;
 	};
 
 	class post : public request {
@@ -32,13 +40,16 @@ namespace http {
 		post() {}
 		virtual ~post() {}
 
-		virtual curl_slist* prepare(const headers_t& headers) const {
-			curl_slist* list = nullptr;
+		void prepare(const headers_t &headers, std::ostream &request_stream) const override {
+			request_stream << "POST /title/ HTTP/1.1 \r\n";
+
 			for (auto h : headers) {
 				std::string s{ std::string(h.first + ": " + h.second) };
-				list = curl_slist_append(list, s.c_str());
+				request_stream << h.first << ": " << h.second << "\r\n";
+				
 			}
-			return list;
+
+			request_stream << "\r\n";
 		}
 	};
 
@@ -46,13 +57,17 @@ namespace http {
 	public:
 		get() {}
 		virtual ~get() {}
-		virtual curl_slist* prepare(const headers_t& headers) const {
-			curl_slist* list = nullptr;
+
+		void prepare(const headers_t &headers, std::ostream &request_stream) const override {
+			request_stream << "GET /title/ HTTP/1.1 \r\n";
+
 			for (auto h : headers) {
 				std::string s{ std::string(h.first + ": " + h.second) };
-				list = curl_slist_append(list, s.c_str());
+				request_stream << h.first << ": " << h.second << "\r\n";
+				
 			}
-			return list;
+
+			request_stream << "\r\n";
 		}
 	};
 
@@ -61,8 +76,7 @@ namespace http {
 		std::string recv_data_;
 		std::string recv_header_;
 	public:
-		connection(const std::string url = "")
-		{}
+		connection(const std::string url = "") {}
 		~connection() {}
 
 		void request(const std::string url, const request& r, const std::string& params = "", const headers_t& headers = headers_t(), const REQTYPE &rtype = REQTYPE::GET) {
@@ -71,34 +85,39 @@ namespace http {
 			std::string final_url = url + "?";
 			std::string final_params = params;
 
+			asio::io_service ios;
+			ip::tcp::socket socket(ios);
+
+			asio::streambuf request;
+			std::ostream request_stream(&request);
+
 			if(rtype == http::REQTYPE::GET){
 				final_url +=params;
 			}
 
-			curl_easy_setopt(curl_object::get_instance(), CURLOPT_URL, final_url.c_str());
-			curl_easy_setopt(curl_object::get_instance(), CURLOPT_WRITEDATA, &recv_data_);
-			curl_easy_setopt(curl_object::get_instance(), CURLOPT_HEADERDATA, &recv_header_);
-			curl_easy_setopt(curl_object::get_instance(), CURLOPT_WRITEFUNCTION, write_received_data_to_string);
+			// curl_easy_setopt(curl_object::get_instance(), CURLOPT_URL, final_url.c_str());
+			// curl_easy_setopt(curl_object::get_instance(), CURLOPT_WRITEDATA, &recv_data_);
+			// curl_easy_setopt(curl_object::get_instance(), CURLOPT_HEADERDATA, &recv_header_);
+			// curl_easy_setopt(curl_object::get_instance(), CURLOPT_WRITEFUNCTION, write_received_data_to_string);
 
 			if(rtype == http::REQTYPE::DELETE){
-				curl_easy_setopt(curl_object::get_instance(), CURLOPT_CUSTOMREQUEST, "DELETE");
+				// curl_easy_setopt(curl_object::get_instance(), CURLOPT_CUSTOMREQUEST, "DELETE");
+
 			}
 
 			if(rtype == REQTYPE::POST || rtype == REQTYPE::DELETE){
-				curl_easy_setopt(curl_object::get_instance(), CURLOPT_POSTFIELDS, final_params.c_str());
-				curl_easy_setopt(curl_object::get_instance(), CURLOPT_POSTFIELDSIZE, final_params.size());
+				// curl_easy_setopt(curl_object::get_instance(), CURLOPT_POSTFIELDS, final_params.c_str());
+				// curl_easy_setopt(curl_object::get_instance(), CURLOPT_POSTFIELDSIZE, final_params.size());
 			}
 
-			if(headers.size()>0)
-			{
-				curl_slist* list = r.prepare(headers);
-				curl_easy_setopt(curl_object::get_instance(), CURLOPT_HTTPHEADER, list);
+			if(headers.size() > 0) {
+				r.prepare(headers, request_stream);
 			}
 
 			CURLcode rescode;
 
 			try{
-				rescode = curl_easy_perform(curl_object::get_instance());
+				rescode = ios.run();
 			}
 			catch(std::exception& e){
 				std::cout<<e.what()<<std::endl;
